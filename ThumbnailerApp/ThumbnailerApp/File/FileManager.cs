30b2c7ab5428a64aadcf64b4547cf2962713dff0
bjare21 <jarek.barecki@gmail.com>
2022-04-25 20:15:05 +0200

    FileManager class created.

---
+++ ThumbnailerApp/ThumbnailerApp/File/FileManager.cs
@@ -0,0 +1,39 @@
+﻿namespace File
+{
+    public static class FileManager
+    {
+        private static readonly string[] defaultFilesTypes = {
+            "jpg",
+            "png",
+            "bmp",
+            "gif" };
+        public static IEnumerable<string> LoadFromDirectory(string directoryPath)
+        {
+            IEnumerable<string> files = new List<string>();
+
+            if (!isValidPath(directoryPath)) return files;
+
+            files = Directory.EnumerateFiles(directoryPath, ".")
+                .Where(file =>
+                defaultFilesTypes.Contains(file.ToLower()[^3..]));
+
+            return files;
+        }
+        public static IEnumerable<string> LoadFromDirectory(string directoryPath, string fileType)
+        {
+            IEnumerable<string> files = new List<string>();
+
+            if (!isValidPath(directoryPath)) return files;
+
+            files = Directory.EnumerateFiles(directoryPath, fileType);
+
+            return files;
+        }
+
+        private static bool isValidPath(string directoryPath)
+        {
+            if (!String.IsNullOrEmpty(directoryPath) && Path.IsPathFullyQualified(directoryPath)) return true;
+            return false;
+        }
+    }
+}
